9a7410127e3b10aac6411ddb39638ed9e7b2d6ff
Patryk Jagielski <pjagielski@academic.rrc.ca>
2022-10-04 08:54:24 -0500

    Added dishes_controller with index and show method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'dishes/index'
+  get 'dishes/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :dishes
 end
